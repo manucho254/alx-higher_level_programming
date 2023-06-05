af6b889366c2c774a3aa8d170829faf72c97dda6
manucho254 <manuchoadero@gmail.com>
2023-06-05 13:20:14 +0000

    concatenate strings

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str.split(" ")
+ language that combines remarkable power with very clear syntax".split(" ")
 print(f'{str[5]} {str[6]} {str[12]} {str[0]}')
