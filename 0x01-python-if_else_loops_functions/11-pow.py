8d0979514024626c9e123194cd8db4d5f0ce68f6
manucho254 <manuchoadero@gmail.com>
2023-06-06 16:04:00 +0000

    function to get the power of a number

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+  pow - function that gets the power of a number
+  @a: integer a
+  @b: integer b
+  Return: a power b
+"""
+
+
+def pow(a: int, b: int) -> int:
+    return a ** b
