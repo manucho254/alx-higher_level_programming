316063fb746737da4baddc50abacfe49f798d1ab
manucho254 <manuchoadero@gmail.com>
2023-06-06 12:28:14 +0000

    get the last digit of a number python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,10 +11,12 @@ if number < 0:
 else:
     last_digit = number % 10
 
+message = "and is 0"
 if last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+    print(f"Last digit of {number} is {last_digit} {message}")
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+    message = "and is greater than 5"
+    print(f"Last digit of {number} is {last_digit} {message}")
 if last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
-
+    message = "and is less than 6 and not 0"
+    print(f"Last digit of {number} is {last_digit} {message}")
