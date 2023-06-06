be146cbfc8c69fa0179503bb526a080936f3cd6a
manucho254 <manuchoadero@gmail.com>
2023-06-06 16:46:37 +0000

    program to remove character from string

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+  remove_char_at - function to remove character from string
+  @string: string to remove character
+  @n: index of the string
+
+  Return: new string without the removed character
+"""
+
+
+def remove_char_at(string: str, n: int) -> str:
+
+    new = ""
+    # check if the index is greater
+    # the the length of the string
+    if n > len(string):
+        return string
+
+    for x in string:
+        if x != string[n]:
+            new += x
+
+    return new
