a6dbd4ea38c685fbc09d2ba6924b666021f7ae31
manucho254 <manuchoadero@gmail.com>
2023-06-08 19:23:02 +0000

    program to print letters of the alphabet in uppercase

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-from os import system
-
 if __name__ == "__main__":
-    system("echo '#pythoniscool'")
+    exec("print('#pythoniscool')")
--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for x in range(65, 91):
-    print("{}".format(chr(x)), end="")
-print()
+    print("{}".format(chr(x) if x < 90 else chr(x) + "\n"), end="")
