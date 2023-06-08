b85f1a88502d73e7bf56a8a2dc9330862919b2b8
manucho254 <manuchoadero@gmail.com>
2023-06-08 17:56:33 +0000

    program to add all values in command line arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,10 +2,5 @@
 from sys import argv
 
 if __name__ == "__main__":
-    idx = 0
-    result = 0
-    for x in argv:
-        if idx > 0:
-            result += int(x)
-        idx += 1
-    print("{:d}".format(result))
+    res = sum([int(argv[x]) for x in range(1, len(argv))])
+    print("{:d}".format(res))
