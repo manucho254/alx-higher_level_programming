bf15e9b9756ae6976817076600ec39918db54260
manucho254 <manuchoadero@gmail.com>
2023-06-08 11:18:57 +0000

    updates to readme

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
-from add_0 import add
+import add_0 as add
 
 a = 1
 b = 2
+
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add.add(a, b)))
