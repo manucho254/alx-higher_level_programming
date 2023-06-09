996490c8fcdf54f86882328eedded7f515b37a7a
manucho254 <manuchoadero@gmail.com>
2023-06-09 09:52:16 +0000

    function to print intergers in list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list: list = []) -> None:
+    """ function to print all intergers in a list
+        Args:
+            my_list: a list of integers
+
+        Return:
+              None
+    """
+    for x in my_list:
+        print("{}".format(x))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,3 @@
+#### Data Structures: Lists, Tuples
+
+- In this set of projects, we will be looking at how to work with lists and tuples in python.
