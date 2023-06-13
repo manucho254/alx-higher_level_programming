3308075f45c664d15e6493a8a6b62504a9f5058e
manucho254 <manuchoadero@gmail.com>
2023-06-13 18:55:46 +0000

    delete item at specified index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -14,7 +14,7 @@ def delete_at(my_list: list = [], idx: int = 0) -> list:
 
     length = len(my_list)
 
-    if idx < 0 or idx > length:
+    if idx < 0 or idx > length or length == 0:
         return my_list
 
     del my_list[idx]
--- 0x03-python-data_structures/11-main.py
+++ 0x03-python-data_structures/11-main.py
@@ -6,3 +6,8 @@ idx = 3
 new_list = delete_at(my_list, idx)
 print(new_list)
 print(my_list)
+my_list = []
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
