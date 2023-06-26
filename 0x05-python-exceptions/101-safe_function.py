b10c811ee84a53034917186d87eaf9aaad39c9ba
manucho254 <manuchoadero@gmail.com>
2023-06-26 12:51:22 +0000

    function that executes a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    """ function that executes a function safely.
+       Args:
+            fct: pointer to function
+            args: function arguments
+
+       Return:
+             the result after invoking fct Otherwise,
+             returns None if something happens during,
+             the function and prints in stderr,
+             the error precede by Exception:
+    """
+    res = None
+    try:
+        res = fct(args[0], args[1])
+    except Exception as e:
+        print(f"Exception: {e}", file=stderr)
+
+    return res
