3c13b96662ba2a50131382aa211c2ca61aaea17d
manucho254 <manuchoadero@gmail.com>
2023-06-27 12:06:59 +0000

    added class method area to Square class to calculate the the current square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square
+
+   Attributes:
+            __size: Private instance attribute
+"""
+
+
+class Square:
+    """class Square that defines a square.
+
+       Attributes:
+            __size: Private instance attribute
+    """
+
+    def __init__(self, size: int = 0) -> None:
+        """ Initializes the instance based on size
+
+            Desc:
+                size must be an integer,
+                otherwise raise a TypeError exception,
+                with the message size must be an integer.
+
+                if size is less than 0,
+                raise a ValueError exception,
+                with the message size must be >= 0.
+
+            Args:
+                size: The size of a square.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self) -> int:
+        """ get the current square area
+
+            Return:
+                  the current square area
+        """
+        return self.__size * self.__size
