1aebe4635f8a26e39063de801adf424a22aedcae
manucho254 <manuchoadero@gmail.com>
2023-07-03 14:16:32 +0000

    program to print square and doc tests

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" function that prints a square with the character #
+   Args:
+       size: the dimension of the square
+"""
+
+def print_square(size):
+    """ print a square with the character #
+        Args:
+            size: the dimension of a square
+    """
+
+    if not isinstance(size, (int)):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
