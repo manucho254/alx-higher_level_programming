f6089f1e827128ec29e6f04b0a3bb650e19ffaae
manucho254 <manuchoadero@gmail.com>
2023-07-03 11:36:37 +0000

    program to add two integers plus tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98) -> int:
+    """ get sum of two integers
+        Args:
+            a: value a can be an integer or float
+            b: value be can be an integer or float
+
+        Return:
+              an integer representing the sum of a and b
+    """
+
+    # check if a is not an int or a float
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    # check if b is not an int or a float
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    # type cast a
+    if isinstance(a, float):
+        a = int(a)
+    # type cast b
+    if isinstance(b, float):
+        b = int(b)
+
+    return a + b
