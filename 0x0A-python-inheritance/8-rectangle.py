04d2b16863cd8dfa481d9ac6c618f3d768f5e4cb
manucho254 <manuchoadero@gmail.com>
2023-07-10 22:40:26 +0000

    Class Rectangle that inherits from BaseGeometry

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -19,7 +19,10 @@ class BaseGeometry:
                 name: arg name
                 value: arg value
         """
-        if not isinstance(value, int) or type(value) != int:
+        val_type = type(value)
+        is_instance = isinstance(value, int)
+        is_subclass = issubclass(val_type, int)
+        if not is_instance or val_type != int or not is_subclass:
             raise TypeError("<name> must be an integer")
         if value <= 0:
             raise ValueError("<name> must be greater than 0")
