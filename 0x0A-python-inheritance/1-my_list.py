aaf2be71b75630380d9eb1fa91ce32f892d29cf8
manucho254 <manuchoadero@gmail.com>
2023-07-10 17:58:15 +0000

    inherit from list and print list in ascending sorted order

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" class that inherits from list
+"""
+
+
+class MyList(list):
+    """ defines MyList and inherits from list
+    """
+    def print_sorted(self):
+        """ print list sorted in ascending order
+        """
+        print(sorted(self))
