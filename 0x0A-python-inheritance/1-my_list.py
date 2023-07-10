cef7da6c8171c64dd1f47969cd8ebed3c5f8c54e
manucho254 <manuchoadero@gmail.com>
2023-07-10 20:34:23 +0000

    inherit from list and print in ascending sort

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -11,6 +11,10 @@ class MyList(list):
         """ print list sorted in ascending order
         """
 
+        if any(not isinstance(x, int) for x in self):
+            print(self)
+            return
+
         arr = self.copy()
         arr.sort()
         print(arr)
