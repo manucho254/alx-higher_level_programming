7ae7b869f6e8d967f0506eb5fb2e4abc43f42327
manucho254 <manuchoadero@gmail.com>
2023-07-11 21:34:37 +0000

    script that reads stdin line by line and computes metrics

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+""" module Write a script that reads stdin line by line and computes metrics.
+"""
+import sys
+import json
+
+
+def get_metrics():
+    """ script that reads stdin line by line and computes metrics
+    """
+    file_size = 0
+    line_count = 0
+
+    status_codes = {
+            "200": 0,
+            "301": 0,
+            "400": 0,
+            "401": 0,
+            "403": 0,
+            "404": 0,
+            "405": 0,
+            "500": 0
+            }
+
+    try:
+        for line in sys.stdin:
+            for j in status_codes:
+                if j in line:
+                    status_codes[j] += 1
+            status = line.strip("\n").split(" ")
+            if line_count > 0 and line_count % 10 == 0:
+                print(f"File size: {file_size}")
+                for x, y in status_codes.items():
+                    print(f"{x}: {y}")
+
+            file_size += int(status[-1])
+            line_count += 1
+
+    except KeyboardInterrupt:
+        print(f"File size: {file_size}")
+        for x, y in status_codes.items():
+            print(f"{x}: {y}")
+        raise
+
+
+if __name__ == "__main__":
+    get_metrics()
