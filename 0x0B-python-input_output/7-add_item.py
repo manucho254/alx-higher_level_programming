4b7dfc304fac69f3056a2e71c3dd801371d63105
manucho254 <manuchoadero@gmail.com>
2023-07-11 22:03:12 +0000

    script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,25 +9,21 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_to_file():
-    """ add all command line arguments to python list
-    """
-    args = sys.argv
-    arr = []  # initialize array
-    filename = "add_item.json"
-    num_args = len(args)
+""" add all command line arguments to python list
+"""
+args = sys.argv
+arr = []  # initialize array
+filename = "add_item.json"
+num_args = len(args)
 
-    # check for arguments
-    if num_args > 1:
-        for x in range(1, num_args):
-            arr.append(args[x])
+# check for arguments
+if num_args > 1:
+    for x in range(1, num_args):
+        arr.append(args[x])
 
-    try:
-        data = load_from_json_file(filename)
-        data.extend(arr)
-        save_to_json_file(data, filename)
-    except Exception as e:
-        save_to_json_file(arr, filename)
-
-
-add_to_file()
+try:
+    data = load_from_json_file(filename)
+    data.extend(arr)
+    save_to_json_file(data, filename)
+except Exception as e:
+    save_to_json_file(arr, filename)
