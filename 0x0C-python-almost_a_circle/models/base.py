8aafed179c27e3b90d527e4518db79655c0f9981
manucho254 <manuchoadero@gmail.com>
2023-07-18 12:50:47 +0000

    updates to to json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,7 +33,7 @@ class Base():
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
-            return json.dumps(list_dictionaries)
+            return str(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
