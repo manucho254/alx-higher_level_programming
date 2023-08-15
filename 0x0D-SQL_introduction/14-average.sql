6c1eab2688ed02b5cb0f82c76a5acf7835c5534f
manucho254 <manuchoadero@gmail.com>
2023-08-15 23:18:28 +0000

    script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT SUM(score)/COUNT(*) AS average from second_table;
+SELECT SUM(score)/COUNT(*) AS average FROM second_table;
