5f895438ebc5c40c3137951edc40d9393cadc4ed
manucho254 <manuchoadero@gmail.com>
2023-09-11 17:35:26 +0000

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,21 +3,15 @@ const process = require('process');
 
 const num = process.argv[2];
 const result = Number.parseInt(num);
-
-// function that print result times
-function printX () {
-  for (let y = 0; y < result; y++) {
-    if (y === result - 1) {
-      console.log('x');
-    } else {
-      process.stdout.write('x');
-    }
-  }
-}
+let x, y, word;
 
 if (Number.isInteger(result)) {
-  for (let x = 0; x < result; x++) {
-    printX();
+  for (x = 0; x < result; x++) {
+    word = '';
+    for (y = 0; y < result; y++) {
+      word += 'x';
+    }
+    console.log(word);
   }
 } else {
   console.log('Missing size');
