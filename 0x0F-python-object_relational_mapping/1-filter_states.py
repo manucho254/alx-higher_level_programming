402c05042eab94eca6e2239114b7ed5ff826cc82
manucho254 <manuchoadero@gmail.com>
2023-09-16 13:30:26 +0000

    Using like Binary instead of like for casem sensitivity

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
                            db=f"{args[3]}", charset="utf8")
     cur = conn.cursor()
     # list all states in my database
-    cur.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
+    cur.execute('SELECT * FROM states WHERE name LIKE BINARY "N%" ORDER BY id ASC')
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
