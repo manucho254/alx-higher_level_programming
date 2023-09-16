48724ea5c2c0060f4f419d82a17dd2e29b905f7a
manucho254 <manuchoadero@gmail.com>
2023-09-16 21:44:16 +0000

    fixes to style errors

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+   script that prints the State object with the name passed,
+   as argument from the database hbtn_0e_6_usa
+    - Script should take 4 arguments: mysql username,
+      mysql password,
+      database name and state name to search (SQL injection free).
+    - Import State and Base from model_state -
+      from model_state import Base, State
+    - Script should connect to a MySQL server running on localhost at port 3306
+    - You can assume you have one record with the state name to search
+    - Results must display the states.id
+    - If no state has the name you searched for, display Not found
+"""
+
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    args = sys.argv  # list of arguments
+    my_db = 'mysql+mysqldb://{}:{}@localhost:3306/{}\
+            '.format(args[1], args[2], args[3])
+    engine = create_engine(my_db, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+
+    # create a session
+    session = Session()
+
+    # get object with name in args
+    state = session.query(State).filter(State.name == "%s" % args[4]).first()
+
+    # if no state is found
+    if not state:
+        print("Not found")
+    else:
+        # print state id
+        print(state.id)
+
+    session.close()
--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-""" 
-  script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
-    - Your script should take 3 arguments: mysql username, mysql password and database name
-    - You must import State and Base from model_state - from model_state import Base, State
-    - Your script should connect to a MySQL server running on localhost at port 3306
+"""
+  script that lists all State objects that contain the
+  letter a from the database hbtn_0e_6_usa
+    - Your script should take 3 arguments:
+      mysql username, mysql password and database name
+    - You must import State and Base from model_state
+      from model_state import Base, State
+    - Your script should connect to a MySQL server
+      running on localhost at port 3306
     - Results must be sorted in ascending order by states.id
 """
 
@@ -17,7 +21,8 @@ from model_state import Base, State
 if __name__ == "__main__":
 
     args = sys.argv  # list of arguments
-    my_db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(args[1], args[2], args[3])
+    my_db = 'mysql+mysqldb://{}:{}@localhost:3306/{}\
+            '.format(args[1], args[2], args[3])
     engine = create_engine(my_db, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine)
@@ -25,8 +30,11 @@ if __name__ == "__main__":
     # create a session
     session = Session()
 
-    # get all objects that contain a 
-    states = session.query(State).filter(State.name.ilike('%a%')).order_by(State.id)
+    # get all objects that contain a
+    states = (session.query(State)
+              .filter(State.name.ilike('%a%'))
+              .order_by(State.id)
+              )
 
     # print nothing if state not found else print first
     for state in states:
