3eb79d31efdf0254bddcaffea1c21c667c5eba53
manucho254 <manuchoadero@gmail.com>
2023-09-28 17:18:33 +0000

    script that sends a DELETE request to the URL passed as the first argument and displays the body of the response

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sL "$1" | grep -i Allow | cut -d ":" -f 2 | xargs
+curl -sIL "$1" | grep "Allow" | cut -d ":" -f 2 | xargs
