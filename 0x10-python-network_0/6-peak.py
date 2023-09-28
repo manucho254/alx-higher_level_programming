75c381cd904833701f2798ac938617f365ddcd6c
manucho254 <manuchoadero@gmail.com>
2023-09-28 20:36:25 +0000

    function that finds a peak in a list of unsorted integers.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,7 +6,7 @@
 def find_peak(list_of_integers: list) -> int:
     """ find peak in a list
         Args:
-            list_of_integers: an list_of_integers of lists
+            list_of_integers: an array of lists
     """
     # base case
     if list_of_integers is None or len(list_of_integers) == 0:
@@ -14,26 +14,9 @@ def find_peak(list_of_integers: list) -> int:
 
     length = len(list_of_integers)
 
-    left = 0
-    right = length - 1
+    for x in range(length - 1):
+        if list_of_integers[x] >= list_of_integers[x - 1]\
+                and list_of_integers[x] >= list_of_integers[x + 1]:
+            return list_of_integers[x]
 
-    while (right <= right):
-        # finding mid by binary right shifting.
-        mid = (left + right) >> 1
-
-        # first case if mid is the answer
-        less_equal_right = list_of_integers[mid - 1] <= list_of_integers[mid]
-        less_equal_left = list_of_integers[mid + 1] <= list_of_integers[mid]
-        if ((mid == 0 or less_equal_right) and
-                (mid == length - 1 or less_equal_left)):
-            break
-
-        # move the right pointer
-        if (mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]):
-            right = mid - 1
-
-        # move the left pointer
-        else:
-            left = mid + 1
-
-    return list_of_integers[mid]
+    return None
