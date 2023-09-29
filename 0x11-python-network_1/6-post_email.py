e02bbf7d21137af4badc900eca5d054cb6da96b3
manucho254 <manuchoadero@gmail.com>
2023-09-29 13:59:43 +0000

    script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" script that takes in a URL and an email address,
+    sends a POST request to the passed URL with the
+    email as a parameter, and finally displays the
+    body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    response = requests.post(args[1], data={"email": args[2]})
+
+    print(response.text)
