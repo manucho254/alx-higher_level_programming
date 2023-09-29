8fd507a54d4240cd49658bb33830c72860efa919
manucho254 <manuchoadero@gmail.com>
2023-09-29 10:49:36 +0000

    script that fetches https://alx-intranet.hbtn.io/status using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Script that fetches https://alx-intranet.hbtn.io/status
 """
-from urllib.request import Request, urlopen
+from urllib.request import urlopen
 
 
 if __name__ == "__main__":
