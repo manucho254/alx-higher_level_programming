41a1b0311d2ebd1a75a9b6a7a4a26f5b37325c8d
manucho254 <manuchoadero@gmail.com>
2023-09-29 10:44:41 +0000

    script that fetches https://alx-intranet.hbtn.io/status using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Script that fetches https://alx-intranet.hbtn.io/status
+"""
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    url = Request('https://alx-intranet.hbtn.io/status')
+    with urlopen(url) as response:
+        page_obj = response.read()
+
+    print("Body response:")
+    print("    - type: {}".format(type(page_obj)))
+    print("    - content: {}".format(page_obj))
+    content = str(page_obj).strip('b').strip("'")
+    print("    - utf8 content: {}".format(content))
