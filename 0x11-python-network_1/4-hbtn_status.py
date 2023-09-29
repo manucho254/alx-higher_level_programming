4c2fb176f7a469e3e3dea9ec03c3194a03c8cd0e
manucho254 <manuchoadero@gmail.com>
2023-09-29 13:32:03 +0000

    script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+
+    print("Body response:")
+    print("   - type: {}".format(type(response.text)))
+    print("   - content: {}".format(response.text))
