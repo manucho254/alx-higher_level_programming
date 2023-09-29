8b6c891776f415aa9ada2024e7fbb465c43558c0
manucho254 <manuchoadero@gmail.com>
2023-09-29 13:55:04 +0000

    script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         page_obj = response.read()
 
         print("Body response:")
-        print("    - type: {}".format(type(page_obj)))
-        print("    - content: {}".format(page_obj))
-        print("    - utf8 content: {}".format(page_obj.decode("utf-8")))
+        print("\t- type: {}".format(type(page_obj)))
+        print("\t- content: {}".format(page_obj))
+        print("\t- utf8 content: {}".format(page_obj.decode("utf-8")))
