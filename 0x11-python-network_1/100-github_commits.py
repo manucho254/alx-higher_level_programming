43a3573d2ed4566368efa6931514512e8d25862d
manucho254 <manuchodero@gmail.com>
2023-09-29 16:59:45 +0000

    script that list 10 current commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,18 +9,25 @@ import sys
 
 if __name__ == "__main__":
     args = sys.argv
-    url = f"https://api.github.com/repos/\
-            {args[2]}/{args[1]}/commits?per_page=10"
 
-    # send request to get commit data
-    response = requests.get(url, headers={})
+    path = f"https://api.github.com/repos/"
+    args = f"{args[2]}/{args[1]}/commits?per_page=10"
+    url = path + args
+
+    if len(args) > 3:
+        # create a session
+        session = requests.Session()
+
+        # set username and password
+        session.auth = (args[3], args[4])
+
+        response = session.get(url, headers={})
+    else:
+        # send request to get commit data
+        response = requests.get(url, headers={})
 
     data = response.json()
 
     for commit in data:
-        user_name = commit.get('author').get('login')
-        url = f"https://api.github.com/users/{user_name}"
-        user = requests.get(url)
-        user_data = user.json()
-
-        print(commit.get("sha"), user_data.get("name"))
+        full_name = commit.get('commit').get('author').get("name")
+        print(commit.get("sha"), full_name)
