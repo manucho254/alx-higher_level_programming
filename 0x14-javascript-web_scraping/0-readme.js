fce99f4639eb65b31d912147a74502bba4fc46d6
manucho254 <manuchoadero@gmail.com>
2023-10-24 10:24:27 +0000

    script that reads and prints the content of a file.

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+/*
+ * script that reads and prints the content of a file.
+ *
+ *  The first argument is the file path.
+ *  The content of the file must be read in utf-8
+ *  If an error occurred during the reading,
+ *  print the error object.
+ */
+
+const process = require('process');
+const fs = require('fs');
+const args = process.argv;
+
+fs.readFile(args[2], 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,12 @@
+### JavaScript - Web scraping
+
+- Learning how to do web scraping with Javascript.
+
+##### Learning Objectives
+
+General:
+
+   - Why JavaScript programming is amazing
+   - How to manipulate JSON data
+   - How to use request and fetch API
+   - How to read and write a file using fs module
