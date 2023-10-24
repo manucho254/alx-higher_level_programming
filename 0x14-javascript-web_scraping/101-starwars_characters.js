592a376328ac6cfd8bb44bb07279bee9d6505abe
manucho254 <manuchoadero@gmail.com>
2023-10-24 20:40:49 +0000

    script that prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -13,15 +13,39 @@ const process = require('process');
 const args = process.argv;
 const url = `https://swapi-api.alx-tools.com/api/films/${args[2]}`;
 
+/*
+ * function sends a GET request to a single URL
+ * and returns a Promise that resolves
+ * with the URL and the response body.
+ */
 function getCharacter (url) {
-  request(url, function (err, resp, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      const data = JSON.parse(body);
-      console.log(data.name);
-    }
+  return new Promise((resolve, reject) => {
+    request(url, (err, resp, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ url, body });
+      }
+    });
   });
+}
+
+/*
+ * we create an array of Promises, one for each URL,
+ * by calling the getCharacter function.
+ */
+function sendGetRequests (urls) {
+  const promises = [];
+
+  for (const url of urls) {
+    promises.push(getCharacter(url));
+  }
+
+  /* Promise.all to wait for all the requests to complete.
+   * This ensures that the responses are in the
+   * same order as the URLs in the input array.
+  */
+  return Promise.all(promises);
 }
 
 request(url, function (err, resp, body) {
@@ -31,8 +55,14 @@ request(url, function (err, resp, body) {
     const data = JSON.parse(body);
     const characters = data.characters;
 
-    characters.forEach(url => {
-      setTimeout(getCharacter, 10000, url);
-    });
+    sendGetRequests(characters)
+      .then(responses => {
+        for (const response of responses) {
+          const data = JSON.parse(response.body);
+          console.log(data.name);
+        }
+      }).catch(err => {
+        console.log(err);
+      });
   }
 });
